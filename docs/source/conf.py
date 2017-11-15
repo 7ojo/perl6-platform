605e87b9a71f2f4af3cbc7a0fd017f888610aec5
Jarkko Haapalainen <jarkko@viidakko.fi>
2017-11-15 15:56:08 +0200

    docs: reverted four and three

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinxcontrib.plantuml']
+#plantuml = 'plantuml'
 plantuml = 'GRAPHVIZ_DOT="/usr/local/opt/graphviz/bin/dot" exec java -jar /usr/share/plantuml/plantuml.jar'
 
 # Add any paths that contain templates here, relative to this directory.
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -14,9 +14,11 @@ Here you'll find Platform's documentation. Tool for quickly define and launch ex
    user -> (use PlantUML)
 
    note left of user
-      Hello Again Part Three!
+      Hello Again!
    end note
    @enduml
+
+.. image:: images/uml/example.png
 
 .. toctree::
    :maxdepth: 2
